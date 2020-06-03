2d4638fb05e5fdf5e1b544f17125006059839e14
Stuart Rowlands <stuart@firecannon.com>
2020-06-04 09:30:53 +1200

    Stringify failures, bump default int.

--- src/commands/crawl.js
+++ src/commands/crawl.js
@@ -67,15 +67,15 @@ function writeResumeState() {
     console.log(chalk.bold.green('✅ DONE: Wrote resume state to ' + resumeFile));
 
 
-    fs.writeFile("last-run-failures.json", failures, 'utf8', function (err) {
+    fs.writeFile("last-run-failures.json", JSON.stringify(failures), 'utf8', function (err) {
       if (err) {
         console.log("An error occured while writing JSON Object to File.");
         return console.log(err);
       }
 
       console.log(chalk.bold.green('✅ DONE: Wrote failure log to last-run-failures.json'));
- 
-    }); 
+
+    });
 
 
 }
@@ -101,7 +101,7 @@ module.exports = async function(argv) {
 
   const domain = argv.domain;
   crawl = crawler(domain);
-  crawl.interval = 300;
+  crawl.interval = 500;
   crawl.maxConcurrency = 3;
   crawl.decodeResponses = true;
   crawl.maxResourceSize = 268435456; // 256MB
@@ -164,7 +164,6 @@ module.exports = async function(argv) {
     failures.push({'code': queueItem.stateData.code,'url': queueItem.url});
 
     if (queueItem.stateData.code == 403) {
-      process.exit();
     }
   });
 
@@ -244,7 +243,7 @@ module.exports = async function(argv) {
             if (err) {
               console.log(chalk.bold.red('❌ ERROR REMOVING TEMPORARY FILE:') + ` ./tmp/${randomFile}`);
             }
-          }); 
+          });
           return;
         }
 
@@ -313,5 +312,5 @@ module.exports = async function(argv) {
   } else {
     crawl.start();
   }
-    
+
 };
