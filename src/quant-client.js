487247d3342dfe864bc5b0b620a396cc044f90a9
steveworley <sj.worley88@gmail.com>
2020-10-14 13:49:45 +1000

    Update the deploy command. (#27)
    * Update the deploy command.
    
    * Remove debug.

--- src/quant-client.js
+++ src/quant-client.js
@@ -264,8 +264,6 @@ const client = function(config) {
       const content = file.toString('utf8');
       location = location.startsWith('/') ? location : `/${location}`;
 
-      location = location.startsWith('/') ? location : `/${location}`;
-
       const options = {
         url: `${config.get('endpoint')}`,
         json: true,
@@ -330,9 +328,6 @@ const client = function(config) {
         },
         formData,
       };
-
-      console.log(local, location);
-      console.log(options);
 
       const res = await post(options);
       return handleResponse(res);
